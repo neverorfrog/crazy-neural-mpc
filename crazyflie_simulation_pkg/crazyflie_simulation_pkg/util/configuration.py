f77287916a6e7dbabd58a8ac6df3ec26b938b3a1
Valerio <spagnoli.valerio.00@gmail.com>
2024-10-23 18:59:56 +0200

    crazyflie simulation

---
+++ crazyflie_simulation_pkg/crazyflie_simulation_pkg/util/configuration.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass, field
+from typing import List
+
+from omegaconf import MISSING
+
+
+@dataclass
+class Position:
+    x: float
+    y: float
+    z: float
+
+
+@dataclass
+class CrazyflieConfig:
+    name: str = MISSING
+    height: float = MISSING
+    initial_position: Position = MISSING
+
+
+@dataclass
+class SwarmConfig:
+    dt: float = field(default=0.01)
+    state_publisher_rate: float = field(default=10.0)
+    led_publisher_rate: float = field(default=1.0)
+    velocity_publisher_rate: float = field(default=1.0)
+    crazyflies: List[CrazyflieConfig] = field(
+        default_factory=list[CrazyflieConfig]
+    )
