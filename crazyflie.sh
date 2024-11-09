1c721162677b5c04a51ffde3b6e4422cb82255c5
neverorfrog <97flavio.maiorana@gmail.com>
2024-11-09 22:44:24 +0100

    two aliases

--- crazyflie.sh
+++ crazyflie.sh
@@ -1,4 +1,5 @@
 alias swarm='ros2 launch crazyflie_swarm_pkg swarm.launch.py'
-alias dock='ros2 launch crazyflie_swarm_pkg dock.launch.py'
+alias teleop='ros2 launch crazyflie_swarm_pkg teleop.launch.py'
+alias simulation='ros2 launch crazyflie_simulation_pkg crazyflie_simulation.launch.py'
 alias takeoff='ros2 service call /take_off crazyflie_swarm_interfaces/srv/TakeOff "{height: 0.25, duration: 1}"'
 alias land='ros2 service call /land crazyflie_swarm_interfaces/srv/Land "{duration: 3}"'
